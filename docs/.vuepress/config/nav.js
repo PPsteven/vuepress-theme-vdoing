06e5933a867983026caf01ab5e860f7011d32328
ppsteven <ppsteven@outlook.com>
2021-06-09 11:50:03 +0800

    latest commit 0609

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,7 +2,7 @@ module.exports = [
   {text: '首页', link: '/'},
   {
     text: '后端',
-    // link: '/backend/',  //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    link: '/backend/',  //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       {text: '后端', items: [
         {text: 'Docker', link: '/pages/e7311f/'}, // 这些link是在相应md文件定义的永久链接。另外，注意结尾是有斜杠的
@@ -22,12 +22,23 @@ module.exports = [
   },
   {
     text: '数据结构与算法', 
-    // link: '/algorithm/',
+    link: '/algorithm/',
     items: [
-      {text: '数据结构', items: [
+      {text: '算法', items: [
         {text: '贪心算法', link: '/pages/b248c0/'},
         {text: '双指针与滑动窗口', link: '/pages/9410cd/'},
+        {text: '二分查找', link: '/pages/025a96/'},
+        {text: '排序算法', link: '/pages/bdcb28/'},
+        {text: '搜索算法', link: '/pages/ec8860/'},
+        {text: '动态规划', link: '/pages/0009c5/'},
+        {text: '系列问题', link: '/pages/5eba5e/'},
        ]},
+      {text: '数据结构', items: [
+        {text: '链表', link: '/pages/c5378b/'},
+        {text: '树', link: '/pages/2723b5/'},
+        {text: '栈和队列', link: '/pages/83762d/'},
+        {text: '并查集', link: '/pages/72acfe/'},
+      ]},
     ]
   },
   {
@@ -42,7 +53,7 @@ module.exports = [
   },
   {
     text: 'Python',
-    // link: '/python/',
+    link: '/python/',
     items: [
       {text: 'Python', items: [
         {text: 'Python', 'link': '/pages/a583c4/'},
@@ -51,23 +62,27 @@ module.exports = [
   },
   {
     text: 'CheetSheet',
-    // link: '/CheetSheet/',
+    link: '/CheetSheet/',
     items: [
       {text: 'docker', link: '/pages/3f6ec3/'},
       {text: 'git', link: '/pages/752e0b/'},
+      {text: 'Linux', link: '/pages/9037d9/'},
+      {text: 'SQL', link: '/pages/58af8c/'},
+      {text: 'Vim', link: '/pages/5b4ee1/'},
+      {text: 'Tmux', link: '/pages/0871b7/'},
     ]
   },
   {text: '环境配置', link: '/pages/1886cc/'},
   {text: '关于', link: '/about/'},
-  {
-    text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
-    items: [
-      {text: '网站', link: '/pages/beb6c0bd8a66cea6/'},
-      {text: '资源', link: '/pages/eee83a9211a70f9d/'},
-      {text: 'Vue资源', link: '/pages/12df8ace52d493f6/'},
-    ]
-  },
+  // {
+  //   text: '收藏',
+  //   link: '/pages/beb6c0bd8a66cea6/',
+  //   items: [
+  //     {text: '网站', link: '/pages/beb6c0bd8a66cea6/'},
+  //     {text: '资源', link: '/pages/eee83a9211a70f9d/'},
+  //     {text: 'Vue资源', link: '/pages/12df8ace52d493f6/'},
+  //   ]
+  // },
   {
     text: '其它',
     link: '/pages/beb6c0bd8a66cea6/',
